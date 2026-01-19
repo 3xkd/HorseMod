e47f3b9d6c52a6a3950893396e89843669286b35
SimKDT <simon.cadet79@hotmail.fr>
2026-01-19 23:14:55 +0100

    Improve action blocker python script to directly output to the correct file

--- Scripts/formatTimedActionBlocker.py
+++ Scripts/formatTimedActionBlocker.py
@@ -15,11 +15,18 @@ csv_path = os.path.join(script_dir, 'timedActions.csv')
 # Read the CSV file using pandas
 df = pd.read_csv(csv_path, encoding='utf-8')
 
-TIMEDACTION_TEMPLATE = """    ---Valid timed actions while horse riding.
+TIMEDACTION_TEMPLATE = """---AUTOMATICALLY GENERATED FROM script/formatTimedActionBlocker.py
+---
+---Holds the data of the timed actions to allow while mounted on a horse.
+local ActionBlocker = {{
+    ---Valid timed actions while horse riding.
     ---@type table<string, true>
     validActions = {{
 {timedActions}
-    }},"""
+    }},
+}}
+
+return ActionBlocker"""
 
 
 # Format the matches into Lua table format
@@ -27,6 +34,7 @@ DATA_FORMAT = '        ["{timedAction}"] = {bool},\n'
 # DATA_FORMAT = '{timedAction}\n'
 
 formatted_actions = ""
+df = df.sort_values(by=['timedAction'])
 for _, row in df.iterrows():
     action = row['timedAction']
     canRun = row['canRun']
@@ -37,6 +45,22 @@ formatted_actions = formatted_actions.rstrip()  # Remove trailing newline
 format_table = TIMEDACTION_TEMPLATE.format(timedActions=formatted_actions)
 
 print(format_table)
-pyperclip.copy(format_table)
 
+output_path = os.path.join(
+    script_dir, 
+    '..', 
+    'Contents',
+    'mods',
+    'HorseMod',
+    '42',
+    'media',
+    'lua',
+    'shared',
+    'HorseMod',
+    'patches',
+    'ActionBlocker.lua'
+)
 
+with open(output_path, 'w', encoding='utf-8') as f:
+    f.write(format_table)
+    print(f"Wrote to \033[36m{output_path}\033[0m")
